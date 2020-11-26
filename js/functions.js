5e7c6f86e01a816f690cfd8c3da127d862e6e2cb
Rizky Hanifudin <41834573+rizky-hanifudin@users.noreply.github.com>
2020-11-26 09:25:07 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -48,3 +48,22 @@ const displayBooks = (books) => {
 
 loadBooks();
 
+function switcher(){
+  const mvr = document.getElementById('mover');
+  const btn = document.getElementById('button');
+  const body = document.getElementsByTagName("BODY")[0];
+  
+  if(btn.className == "button day"){
+    mvr.style.transform = "translateY(-42px)";
+    btn.className = "button night";
+    btn.style.boxShadow = "0px 0px 16px rgba(255, 255, 255, 0.25)";
+    body.style.backgroundColor = "#0E0E0E";
+    body.style.color = "white";
+  } else{
+    mvr.style.transform = "translateY(0)";
+    btn.className = "button day";
+    btn.style.boxShadow = "0px 0px 16px rgba(0, 0, 0, 0.25)";
+    body.style.backgroundColor = "white";
+    body.style.color = "Black";
+  }
+}
